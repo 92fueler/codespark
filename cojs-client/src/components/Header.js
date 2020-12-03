d8d66ec2abc1d6f61dd7f042c563c00fea46865e
Joyce Wu <jianjoycewu@gmail.com>
2020-12-02 17:11:54 -0800

    navbar included in the header component

--- cojs-client/src/components/Header.js
+++ cojs-client/src/components/Header.js
@@ -1,7 +1,23 @@
 import React from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
 
 const Header = () => {
-  return <div>this is header</div>;
+  return (
+    <header>
+      <Navbar bg="light" expand="lg" collapseOnSelect>
+        <Container>
+          <Navbar.Brand href="/">CodeSpark</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ml-auto">
+              <Nav.Link href="/signin">Sign In</Nav.Link>
+              <Nav.Link href="/signup">Sign Up</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
 };
 
 export default Header;
