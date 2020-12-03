b80db5fce8ebd4fde9f0f6a59c77c73021fc8ea1
Joyce Wu <jianjoycewu@gmail.com>
2020-12-02 17:15:47 -0800

    font-awesome added by cdn link, icons added to header component

--- cojs-client/src/components/Header.js
+++ cojs-client/src/components/Header.js
@@ -10,8 +10,12 @@ const Header = () => {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ml-auto">
-              <Nav.Link href="/signin">Sign In</Nav.Link>
-              <Nav.Link href="/signup">Sign Up</Nav.Link>
+              <Nav.Link href="/signin">
+                <i className="fas fa-sign-in-alt"></i> Sign In
+              </Nav.Link>
+              <Nav.Link href="/signup">
+                <i className="fas fa-user"></i> Sign Up
+              </Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
