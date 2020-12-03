bf52dec83407eb9cfe9eb6af3363ef41a8674825
Joyce Wu <jianjoycewu@gmail.com>
2020-12-03 01:04:13 -0800

    install bcyptjs and hash password for users

---
+++ cojs-server/data/users.js
@@ -0,0 +1,22 @@
+import bcypt from "bcryptjs";
+
+const users = [
+  {
+    name: "Admin User",
+    email: "admin@example.com",
+    password: bcrypt.hashSync("123456", 10),
+    isAdmin: true,
+  },
+  {
+    name: "John Doe",
+    email: "john@example.com",
+    password: bcrypt.hashSync("123456", 10),
+  },
+  {
+    name: "Adam Ng",
+    email: "jeff@example.com",
+    password: bcrypt.hashSync("123456", 10),
+  },
+];
+
+export default users;
