23c2c1eebbe4c76fd3eb6a9918b876266645b141
Joyce Wu <jianjoycewu@gmail.com>
2020-12-03 00:07:23 -0800

    install dotenv and mongoose and connected

--- cojs-server/server.js
+++ cojs-server/server.js
@@ -1,9 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
+import connectDB from "./config/db.js";
 import problems from "./data/problems.js";
-const app = express();
 
 dotenv.config();
+
+connectDB();
+
+const app = express();
 
 app.get("/", (req, res) => {
   res.send("api is running");
