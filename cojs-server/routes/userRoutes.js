56291a0631e494a87e43c60108a0ae12c8b94239
Joyce Wu <jianjoycewu@gmail.com>
2020-12-04 00:28:42 -0800

    custom authentication middleware

--- cojs-server/routes/userRoutes.js
+++ cojs-server/routes/userRoutes.js
@@ -1,10 +1,13 @@
 import express from "express";
-import { authUser } from "../controllers/userController.js";
+import { authUser, getUserProfile } from "../controllers/userController.js";
+import { protect } from "../middleware/authMiddleware.js";
 const router = express.Router();
 
 // @desc Auth user & get token
 // @route POST /api/v1/users/signin
 // @access public
 router.post("/signin", authUser);
+
+router.route("/profile").get(protect, getUserProfile);
 
 export default router;
