3540dbfdb07af80d8bbaa9c337d2cd02ed6616fa
Joyce Wu <jianjoycewu@gmail.com>
2020-12-04 14:40:40 -0800

    user login reducer and action added

---
+++ cojs-client/src/actions/userAction.js
@@ -0,0 +1,41 @@
+import axios from "axios";
+import {
+  USER_SIGNIN_REQUEST,
+  USER_SIGNIN_SUCCESS,
+  USER_SIGNIN_FAIL,
+} from "../constants/userConstants";
+
+export const signin = (email, password) => async (dispatch) => {
+  try {
+    dispatch({
+      type: USER_SIGNIN_REQUEST,
+    });
+
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+
+    const { data } = await axios.post(
+      "/api/users/login",
+      { email, password },
+      config
+    );
+
+    dispatch({
+      type: USER_SIGNIN_SUCCESS,
+      payload: data,
+    });
+
+    localStorage.setItem("userInfo", JSON.stringify(data));
+  } catch (error) {
+    dispatch({
+      type: USER_SIGNIN_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
--- cojs-client/src/store.js
+++ cojs-client/src/store.js
@@ -5,12 +5,21 @@ import {
   problemListReducer,
   problemDetailsReducer,
 } from "./reducers/problemReducer";
+import { userSigninReducer } from "./reducers/userReducer";
 
 const reducer = combineReducers({
   problemList: problemListReducer,
   problemDetails: problemDetailsReducer,
+  userSignin: userSigninReducer,
 });
-const initialState = {};
+
+const userInfoFromStorage = localStorage.getItem("userInfo")
+  ? JSON.parse(localStorage.getItem("userInfo"))
+  : null;
+
+const initialState = {
+  userLogin: { userInfo: userInfoFromStorage },
+};
 const middleware = [thunk];
 const store = createStore(
   reducer,
