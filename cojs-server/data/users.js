87d845c0a68a2a56c79d0f9e09b289ba36ecdabb
Joyce Wu <jianjoycewu@gmail.com>
2020-12-04 23:18:06 -0800

    add more examples to data seeder files

--- cojs-server/data/users.js
+++ cojs-server/data/users.js
@@ -13,8 +13,53 @@ const users = [
     password: bcrypt.hashSync("123456", 10),
   },
   {
-    name: "Adam Ng",
+    name: "Jeff Ng",
     email: "jeff@example.com",
+    password: bcrypt.hashSync("123456", 10),
+  },
+  {
+    name: "Adam Ng",
+    email: "Adam@example.com",
+    password: bcrypt.hashSync("123456", 10),
+  },
+  {
+    name: "Roy James",
+    email: "roy@example.com",
+    password: bcrypt.hashSync("123456", 10),
+  },
+  {
+    name: "Aman Verma",
+    email: "Aman@example.com",
+    password: bcrypt.hashSync("123456", 10),
+  },
+  {
+    name: "Noman White",
+    email: "noman@example.com",
+    password: bcrypt.hashSync("123456", 10),
+  },
+  {
+    name: "Kevin Huang",
+    email: "Kevin@example.com",
+    password: bcrypt.hashSync("123456", 10),
+  },
+  {
+    name: "David Lowell",
+    email: "david@example.com",
+    password: bcrypt.hashSync("123456", 10),
+  },
+  {
+    name: "Robert Williams",
+    email: "rober@example.com",
+    password: bcrypt.hashSync("123456", 10),
+  },
+  {
+    name: "Mike James",
+    email: "mike@example.com",
+    password: bcrypt.hashSync("123456", 10),
+  },
+  {
+    name: "Joseph Lin",
+    email: "joseph@example.com",
     password: bcrypt.hashSync("123456", 10),
   },
 ];
