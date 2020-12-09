c02f9bf2b3ea69b5b2d8eb9adbca76b0bc541a87
Joyce Wu <jianjoycewu@gmail.com>
2020-12-08 17:38:49 -0800

    add deleteProblem, createProblem, updateProblem on problemController

--- cojs-server/controllers/problemController.js
+++ cojs-server/controllers/problemController.js
@@ -22,4 +22,75 @@ const getProblemById = asyncHandler(async (req, res) => {
   }
 });
 
-export { getProblems, getProblemById };
+// @desc delete a problem
+// @route DELETE /api/v1/problems/:id
+// @access private/admin
+const deleteProblem = asyncHandler(async (req, res) => {
+  const problem = await Problem.findOne({ id: parseInt(req.params.id) });
+  if (problem) {
+    await problem.remove();
+    res.json({ message: "Problem Deleted!" });
+  } else {
+    res.status(404);
+    throw new Error("Problem Not Found!");
+  }
+});
+
+// @desc create a problem
+// @route POST /api/v1/problems
+// @access private/admin
+// TODO: need to rewrite this function
+const createProblem = asyncHandler(async (req, res) => {
+  const { id, title, desc, difficulty } = req.body;
+  const problemById = await Problem.findOne({ id: id });
+  const problemByTitle = await Problem.findOne({ title: title });
+  if (problemById) {
+    throw Error("Problem id is taken");
+  }
+  if (problemByTitle) {
+    throw Error("Problem title exists");
+  }
+
+  const problem = new Problem({
+    id: id,
+    title: title,
+    desc: desc,
+    difficulty: difficulty,
+  });
+  if (problem) {
+    const createdProblem = await problem.save();
+    res.status(201).json(createdProblem);
+  } else {
+    res.status(404);
+    throw new Error("Cannot create problem this time ...");
+  }
+});
+
+// @desc update a problem
+// @route PUT /api/v1/problems
+// @access private/admin
+const updateProblem = asyncHandler(async (req, res) => {
+  const { id, title, desc, difficulty } = req.body;
+
+  const problem = await Problem.findOne({ id: parseInt(req.params.id) });
+  if (problem) {
+    problem.id = id;
+    problem.title = title;
+    problem.desc = desc;
+    problem.difficulty = difficulty;
+
+    const updatedProblem = await problem.save();
+    res.status(201).json(problem);
+  } else {
+    res.status(404);
+    throw new Error("Problem Not Found");
+  }
+});
+
+export {
+  getProblems,
+  getProblemById,
+  deleteProblem,
+  createProblem,
+  updateProblem,
+};
